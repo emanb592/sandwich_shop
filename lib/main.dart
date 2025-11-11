200bb93089305fbf7cbab214ada501d646cafad4
emanb592 <emmanuelbarker05@gmail.com>
2025-11-11 18:18:15 +0000

    Adding buttons

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const App());
 }
+
 class App extends StatelessWidget {
   const App({super.key});
- 
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +13,27 @@ class App extends StatelessWidget {
       title: 'Sandwich Shop App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: const Center(
-          child: OrderItemDisplay(5, 'Footlong'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const OrderItemDisplay(5, 'Footlong'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => print('Add button pressed!'),
+                    child: const Text('Add'),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () => print('Remove button pressed!'),
+                    child: const Text('Remove'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -39,21 +58,16 @@ class OrderItemDisplay extends StatelessWidget {
   }
 }*/
 
-      return Container(
-        color: Colors.amber,
-        child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}'),
-                Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}'),
-                Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}'),
-              ]
-            ),
-      );
-        }
-      }
-      
-
-
-
-
+    return Container(
+      color: Colors.amber,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}'),
+          Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}'),
+          Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}'),
+        ],
+      ),
+    );
+  }
+}
